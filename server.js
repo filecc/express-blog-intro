ea7cd0b0860aef0b0367ddf0512c165a04eb1b1e
filecc <fil.gia@live.it>
2023-11-10 14:39:14 +0100

    Update start script to use nodemon and add post controller

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+const dotenv = require('dotenv').config()
 const express = require('express')
 const app = express()
-
 const port = process.env.PORT
 
+const postController = require('./controllers/post')
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`<h1>Benvenuto nel mio blog</h1> <a href="/posts">Vai ai post</a>`)
 })
 
-app.listen(port || 3000, () => {
+app.get('/posts', postController.index)
+
+app.listen(port ?? 3000, () => {
   console.log(`Server running at http://localhost:${port}`)
 })
